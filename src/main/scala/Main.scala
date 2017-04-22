aaa4a85b1194b8bb7ec378b944fec36997fb8134
Wong <stephane.wong1@gmail.com>
2017-04-23 00:02:46 +0200

    first supervisor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,24 @@
 import akka.actor.{ActorSystem, Props}
-
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration._
 /**
   * Created by wong on 22/04/17.
   */
 object Main extends App {
 
   val system = ActorSystem("simple")
-  val p1 = system.actorOf(Props[TennisPlayer], name = "p1")
-  val p2 = system.actorOf(Props[TennisPlayer], name = "p2")
 
-  p1 ! Service(p2)
+  implicit val timeout = Timeout(5.second)
+
+  val referee = system.actorOf(Props[TennisReferee], name = "referee")
+  referee ! TennisGame("p1","p2")
+  /*val futureWinner = referee ? Won
+  futureWinner.map { a =>
+    println(a)}*/
+  //system.terminate()
 
 
 
--- src/main/scala/SimpleActor.scala
+++ src/main/scala/SimpleActor.scala
@@ -1,17 +1,29 @@
-import akka.actor.{Actor, ActorRef}
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorRef, Props, SupervisorStrategy}
 import akka.event.Logging
 
+import scala.concurrent.Await
+import akka.pattern.ask
+
+import scala.collection.mutable
+import scala.concurrent.duration._
 /**
   * Created by stephane on 22/04/2017.
   */
 
 final case class Service(actor: ActorRef)
+
 final case class Ball(lift: Boolean)
+
 final case object Won
+
+final case object BallMissed
+
+final case class TennisGame(p1: String, p2: String)
 
 class TennisPlayer extends Actor {
 
-  val log = Logging(context.system,this)
+  val log = Logging(context.system, this)
 
   override def receive = {
     case Service(opponent) =>
@@ -19,21 +31,55 @@ class TennisPlayer extends Actor {
       opponent ! Ball(false)
     case Ball(lift) =>
       log.info(s"${self.path}")
-      if(!lift) {
+      if (!lift) {
         sender ! Ball(Math.random() > 0.5)
       }
       else {
-        if(Math.random() > 0.8) {
+        if (Math.random() > 0.8) {
           log.info(s"${self.path} missed the ball")
-          sender ! Won
-          context.stop(self)
+          context.parent ! BallMissed
+          //context.stop(self)
         }
         else {
           sender ! Ball(false)
         }
       }
-    case Won =>
-      log.info(s"${self.path} won")
-      context.stop(self)
+  }
+}
+
+class Supervisor extends Actor {
+
+  import akka.actor.OneForOneStrategy
+  import akka.actor.SupervisorStrategy._
+  import scala.concurrent.duration._
+
+
+  override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy(maxNrOfRetries = 3, withinTimeRange = 1 minute) {
+    case _: IndexOutOfBoundsException => Restart
+  }
+
+  override def receive = {
+    case _: IndexOutOfBoundsException =>
+  }
+}
+
+class TennisReferee extends Actor {
+
+  val log = Logging(context.system, this)
+
+  val list = scala.collection.mutable.ListBuffer[String]()
+
+  override def receive = {
+
+    case TennisGame(p1, p2) =>
+      val player1 = context.actorOf(Props[TennisPlayer], name = p1)
+      val player2 = context.actorOf(Props[TennisPlayer], name = p2)
+      player1 ! Service(player2)
+    case BallMissed =>
+      log.info(s"${sender.path} lost")
+      list.+=(s"${sender.path} won")
+      self ! Won
+      //context.stop(sender)
+    case Won => log.info(Option(list.remove(0)).getOrElse("Nobody yet"))
   }
 }
